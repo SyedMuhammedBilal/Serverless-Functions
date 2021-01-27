3e4006ad98521d85772951e11f13a1a3bfd16a7b
Syed Muhammed Bilal <intergo878@gmail.com>
2021-01-27 19:31:14 +0500

    form added

---
+++ functions/helpers/linkQueries.js
@@ -0,0 +1,46 @@
+const GET_MESSAGES = `
+    query {
+        allMessages {
+            data {
+                _id
+                text
+                archived
+            }
+        }
+    }
+`
+
+const CREATE_MESSAGE = `
+    mutation($text: String!, $archived: Boolean!) {
+        createMessage(data: { text: $text, archived: $archived }) {
+            _id
+            text
+            archived
+        }
+    }
+`
+
+const UPDATE_MESSAGE = `
+    mutation($id: ID!, $text: String!, $archived: Boolean!) {
+        updateMessage(id: $id, data: { text: $text, archived: $archived }) {
+            _id
+            text
+            archived
+        }
+    }
+`
+
+const DELETE_MESSAGE = `
+    mutation($id: ID!) {
+        deleteLink(id: $id) {
+            _id
+        }
+    }
+`
+
+module.exports = {
+    CREATE_MESSAGE,
+    UPDATE_MESSAGE,
+    GET_MESSAGES,
+    DELETE_MESSAGE
+}
